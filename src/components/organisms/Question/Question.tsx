b875237acf0fd2b62916cb85166041cea0b15b0e
Agne Laurinaviciute <agnelaurinaviciute26@gmail.com>
2023-12-16 15:54:43 +0200

    fetched and displayed data on the main page and added some styles

--- src/components/organisms/Question/Question.tsx
+++ src/components/organisms/Question/Question.tsx
@@ -1,7 +1,24 @@
+import React from "react";
 import styles from "./Question.module.css";
 
-const Question = () => {
-  return <div className={styles.question}></div>;
+type QuestionType = {
+  question_text: string;
+  date: string;
+  id: string;
+  user_id: string;
+};
+
+type QuestionComponentType = {
+  question: QuestionType;
+};
+
+const Question: React.FC<QuestionComponentType> = ({ question }) => {
+  return (
+    <div className={styles.question}>
+      <div>{question.question_text}</div>
+      <div className={styles.questionDate}>{question.date}</div>
+    </div>
+  );
 };
 
 export default Question;
--- src/components/organisms/Questions/Questions.tsx
+++ src/components/organisms/Questions/Questions.tsx
@@ -1,11 +1,22 @@
+import React from "react";
 import styles from "./Questions.module.css";
 import Question from "../Question/Question";
 
-const Questions = ({ questions }) => {
-  console.log(questions);
+type QuestionsType = {
+  questions: Array<any> | null;
+};
+
+const Questions: React.FC<QuestionsType> = ({ questions }) => {
   return (
-    <div>
-      <Question />
+    <div className={styles.wrapper}>
+      {questions &&
+        questions.map((question) => {
+          return (
+            <div key={question.id}>
+              <Question question={question} />
+            </div>
+          );
+        })}
     </div>
   );
 };
